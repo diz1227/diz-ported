dc85381fed420f7891bb7b13afe749bbfabcc36e
diz1227 <nmyers0068@gmail.com>
2026-05-16 12:16:59 -0400

    Update ArrayListHudModule.java

--- src/main/java/me/alpha432/oyvey/features/modules/hud/ArrayListHudModule.java
+++ src/main/java/me/alpha432/oyvey/features/modules/hud/ArrayListHudModule.java
@@ -1,14 +1,19 @@
 package me.alpha432.oyvey.features.modules.hud;
 
+import java.awt.Color;
+import me.alpha432.oyvey.features.settings.Setting;
 import me.alpha432.oyvey.OyVey;
 import me.alpha432.oyvey.event.impl.render.Render2DEvent;
 import me.alpha432.oyvey.features.modules.Module;
 import me.alpha432.oyvey.features.modules.client.HudModule;
 
 public class ArrayListHudModule extends HudModule {
+    private final Setting<Color> color =
+        new Setting<>("Color", new Color(255, 255, 255, 255));
 
     public ArrayListHudModule() {
         super("ArrayList", "Displays enabled modules", 5, 30);
+        register(color);
     }
 
     @Override
