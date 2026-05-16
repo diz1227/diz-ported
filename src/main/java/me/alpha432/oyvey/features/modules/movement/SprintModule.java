d31cda74fbe0b1dc5e265d0c16518f06d745cca6
diz1227 <nmyers0068@gmail.com>
2026-05-15 21:04:50 -0400

    Update SprintModule.java

--- src/main/java/me/alpha432/oyvey/features/modules/movement/SprintModule.java
+++ src/main/java/me/alpha432/oyvey/features/modules/movement/SprintModule.java
@@ -1,24 +1,17 @@
 package me.alpha432.oyvey.features.modules.movement;
-
+ 
 import me.alpha432.oyvey.features.modules.Module;
-
+ 
 public class SprintModule extends Module {
-
+ 
     public SprintModule() {
         super("Sprint", "Auto sprints for you.", Category.MOVEMENT);
     }
-
+ 
     @Override
     public void onTick() {
         if (nullCheck()) return;
-
-        boolean isSneaking    = mc.player.isSneaking();
-        boolean isUsingItem   = mc.player.isUsingItem();
-        boolean tooHungry     = mc.player.getHungerManager().getFoodLevel() <= 6;
-        boolean movingForward = mc.player.input != null && mc.player.input.playerInput.forward();
-
-        if (!isSneaking && !isUsingItem && !tooHungry && movingForward) {
-            mc.player.setSprinting(true);
-        }
+ 
+        mc.player.setSprinting(true);
     }
 }
