a962f1e70e4647fcf048f4e48d4a1e8d081b9541
diz1227 <nmyers0068@gmail.com>
2026-05-16 12:12:12 -0400

    Update ArrayListHudModule.java

--- src/main/java/me/alpha432/oyvey/features/modules/hud/ArrayListHudModule.java
+++ src/main/java/me/alpha432/oyvey/features/modules/hud/ArrayListHudModule.java
@@ -1,65 +1,45 @@
 package me.alpha432.oyvey.features.modules.hud;
 
+import me.alpha432.oyvey.OyVey;
 import me.alpha432.oyvey.event.impl.render.Render2DEvent;
 import me.alpha432.oyvey.features.modules.Module;
 import me.alpha432.oyvey.features.modules.client.HudModule;
-import me.alpha432.oyvey.features.settings.Setting;
-import me.alpha432.oyvey.OyVey;
-
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class ArrayListHudModule extends HudModule {
 
-    private final Setting<Float> scale = flt("Scale", 1.0f, 0.5f, 3.0f);
-
     public ArrayListHudModule() {
         super("ArrayList", "Displays enabled modules", 5, 30);
-
-        register(scale);
     }
 
     @Override
     protected void render(Render2DEvent e) {
         super.render(e);
 
-        List<Module> enabledModules = OyVey.moduleManager.modules.stream()
-                .filter(Module::isEnabled)
-                .filter(module -> module != this)
-                .sorted(Comparator.comparingInt(
-                        module -> -mc.font.width(module.getName())
-                ))
-                .collect(Collectors.toList());
-
         int yOffset = 0;
         int maxWidth = 0;
 
-        for (Module module : enabledModules) {
+        for (Module module : OyVey.moduleManager.getModules()) {
+
+            if (!module.isEnabled()) continue;
+            if (module == this) continue;
+
             String text = module.getName();
 
-            int drawX = (int) getX();
-            int drawY = (int) getY() + yOffset;
-
-            e.getContext().pose().pushPose();
-            e.getContext().pose().scale(scale.getValue(), scale.getValue(), 1.0f);
-
-            mc.font.draw(
-                    e.getContext().pose(),
+            e.getContext().drawString(
+                    mc.font,
                     text,
-                    drawX / scale.getValue(),
-                    drawY / scale.getValue(),
+                    (int) getX(),
+                    (int) getY() + yOffset,
                     -1
             );
 
-            e.getContext().pose().popPose();
+            int width = mc.font.width(text);
 
-            int width = (int) (mc.font.width(text) * scale.getValue());
             if (width > maxWidth) {
                 maxWidth = width;
             }
 
-            yOffset += (int) ((mc.font.lineHeight + 2) * scale.getValue());
+            yOffset += mc.font.lineHeight + 2;
         }
 
         setWidth(maxWidth);
