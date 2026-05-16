d1108a4fc34c24b0bc080edf4c59c40f8f74d951
diz1227 <nmyers0068@gmail.com>
2026-05-16 00:17:39 -0400

    Update ModuleManager.java

--- src/main/java/me/alpha432/oyvey/manager/ModuleManager.java
+++ src/main/java/me/alpha432/oyvey/manager/ModuleManager.java
@@ -8,6 +8,7 @@ import me.alpha432.oyvey.event.impl.render.Render3DEvent;
 import me.alpha432.oyvey.features.Feature;
 import me.alpha432.oyvey.features.commands.ModuleCommand;
 import me.alpha432.oyvey.features.modules.Module;
+import me.alpha432.oyvey.features.modules.combat.TriggerBotModule;
 import me.alpha432.oyvey.features.modules.client.ClickGuiModule;
 import me.alpha432.oyvey.features.modules.client.HudEditorModule;
 import me.alpha432.oyvey.features.modules.client.NotificationsModule;
@@ -53,6 +54,7 @@ public class ModuleManager implements Jsonable, Util {
         register(new NoFallModule());
         register(new KeyPearlModule());
         register(new SprintModule());
+        register(new TriggerBotModule());
 
         LOGGER.info("Registered {} modules", modules.size());
 
