d952834348349bba83034d72d570b4efdfa1d0c2
diz1227 <nmyers0068@gmail.com>
2026-05-15 21:01:31 -0400

    Update ModuleManager.java

--- src/main/java/me/alpha432/oyvey/manager/ModuleManager.java
+++ src/main/java/me/alpha432/oyvey/manager/ModuleManager.java
@@ -18,6 +18,7 @@ import me.alpha432.oyvey.features.modules.hud.WatermarkHudModule;
 import me.alpha432.oyvey.features.modules.misc.MCFModule;
 import me.alpha432.oyvey.features.modules.movement.ReverseStepModule;
 import me.alpha432.oyvey.features.modules.movement.StepModule;
+import me.alpha432.oyvey.features.modules.movement.SprintModule;
 import me.alpha432.oyvey.features.modules.player.FastPlaceModule;
 import me.alpha432.oyvey.features.modules.player.NoFallModule;
 import me.alpha432.oyvey.features.modules.player.VelocityModule;
@@ -51,6 +52,7 @@ public class ModuleManager implements Jsonable, Util {
         register(new BlockHighlightModule());
         register(new NoFallModule());
         register(new KeyPearlModule());
+        register(new SpringModule());
 
         LOGGER.info("Registered {} modules", modules.size());
 
